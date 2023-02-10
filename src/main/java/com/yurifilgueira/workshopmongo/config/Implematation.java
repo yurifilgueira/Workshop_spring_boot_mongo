6a3e91f86dddfcf35ace340b85e4326597c7ad83
yurifilgueira <yurimedeiros141@gmail.com>
2023-02-10 15:24:02 -0300

    Data projection of author with DTO

--- src/main/java/com/yurifilgueira/workshopmongo/config/Implematation.java
+++ src/main/java/com/yurifilgueira/workshopmongo/config/Implematation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.yurifilgueira.workshopmongo.domain.Post;
 import com.yurifilgueira.workshopmongo.domain.User;
+import com.yurifilgueira.workshopmongo.dto.AuthorDTO;
 import com.yurifilgueira.workshopmongo.repository.PostRepository;
 import com.yurifilgueira.workshopmongo.repository.UserRepository;
 
@@ -29,15 +30,17 @@ public class Implematation implements CommandLineRunner{
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1  = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1  = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("2018/03/23"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
