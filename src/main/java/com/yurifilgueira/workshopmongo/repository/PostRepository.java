c68577e31d33ca78909386712100c0bd52e960a0
yurifilgueira <yurimedeiros141@gmail.com>
2023-02-10 17:51:50 -0300

    Simple query with query methods

--- src/main/java/com/yurifilgueira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/yurifilgueira/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.yurifilgueira.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.yurifilgueira.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text); 
+	
 }
--- src/main/java/com/yurifilgueira/workshopmongo/services/PostService.java
+++ src/main/java/com/yurifilgueira/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.yurifilgueira.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,4 +21,9 @@ public class PostService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
