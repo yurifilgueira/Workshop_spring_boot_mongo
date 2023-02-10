f439378019ff6f28f5771a078a46d8e73c493eeb
yurifilgueira <yurimedeiros141@gmail.com>
2023-02-10 18:31:13 -0300

    Simple query with @Query

--- src/main/java/com/yurifilgueira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/yurifilgueira/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.yurifilgueira.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.yurifilgueira.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text); 
 	
 }
